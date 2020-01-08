da2723ddaf0fa9e0650bbd9efcd73152549cc6bf
PolarizedIons <polarizedions@gmail.com>
2020-01-08 23:12:54 +0200

    delete command

--- src/api/v1.js
+++ src/api/v1.js
@@ -7,50 +7,44 @@ const router = express.Router();
 
 const TIMEOUT_MS = 10000;
 
-const natsComm = (requestTopic, richResponse, commDesc) => {
+const natsComm = (requestTopic, requestData, richResponse, commDesc) => {
     richResponse.req.log.debug("NATS comm: " + commDesc);
 
-    nats.requestOne(requestTopic, 10000, response => {
+    nats.requestOne(requestTopic, requestData, TIMEOUT_MS, response => {
         if (
             response instanceof NATS.NatsError &&
             response.code === NATS.REQ_TIMEOUT
         ) {
-            req.log.error("NATS Timeout");
-            return richResponse.err("Timeout getting list of commands");
+            richResponse.req.log.error("NATS Timeout");
+            return richResponse.err(`Timeout fetching: ${commDesc}`);
         }
 
         if (!response.success) {
-            req.log.error("Error fetching", response.err);
-            return richResponse.err("Error fetching list of commands");
+            richResponse.req.log.error("Error fetching", response.err);
+            return richResponse.err(`Error fetching: ${commDesc}`);
         }
+
         richResponse.ok(response.data);
     });
 };
 
-router.get("/commands", (req, res) => {
-    natsComm("commandList", new RichResponse(req, res), "list of commands");
-});
-
-// router.get("/commands", (req, res) => {
-//     req.log.debug("Getting list of commands");
-//     const resp = new RichResponse(req, res);
-
-//     nats.requestOne("commandList", 10000, response => {
-//         if (
-//             response instanceof NATS.NatsError &&
-//             response.code === NATS.REQ_TIMEOUT
-//         ) {
-//             req.log.error("NATS Timeout");
-//             return resp.err("Timeout getting list of commands");
-//         }
-
-//         if (!response.success) {
-//             req.log.error("Error fetching", response.err);
-//             return resp.err("Error fetching list of commands");
-//         }
-//         resp.ok(response.data);
-//     });
-// });
+router
+    .get("/commands", (req, res) => {
+        natsComm(
+            "commandList",
+            {},
+            new RichResponse(req, res),
+            "list of commands"
+        );
+    })
+    .delete("/commands/:id", (req, res) => {
+        natsComm(
+            "commandDelete",
+            { id: req.params.id },
+            new RichResponse(req, res),
+            "delete command"
+        );
+    });
 
 module.exports = app => {
     app.use("/v1", router);
